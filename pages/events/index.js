0560f36359ce83fe632f5683cdb3eb421ccdf7fd
Sarah Adewale <saraphynaadewale@gmail.com>
2023-05-19 17:53:02 +0100

    finished about page style

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,21 +1,9 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import AllEvents from '../../src/components/events/events-page';
+
 
 const EventsPage = ({data}) => {
-    return (
-        <div>
-            <h1>Events Page</h1>
-            <div>
-                {data.map((ev) => (
-                    <Link key={ev.id} href={`/events/${ev.id}`} passHref>
-                         <Image src={ev.image} alt={ev.title} width={200} height={100}/> <h2>{ev.title}</h2>
-                    </Link>
-                ))}
-                
-            </div>
-        </div>
-        
-    )
+    return <AllEvents data={data}/>
+    
 }
 
 export default EventsPage; 
